633e4a571e1c0ab18f08974ef1e686bab91d598f
randrand <randrand@users.noreply.github.com>
2013-08-21 17:37:16 -0500

    Update server.py

--- testDBserver/src/server.py
+++ testDBserver/src/server.py
@@ -1,4 +1,3 @@
-
 from google.appengine.ext import ndb
 import cgi 
 import json
@@ -8,18 +7,18 @@ import webapp2
 
 class WeatherEntry(ndb.Model):
   zipcode = ndb.StringProperty()
-  date = ndb.StringProperty()
+  # date = ndb.StringProperty()
   temp = ndb.StringProperty()
   dateTime = ndb.DateTimeProperty(auto_now=True)
 
 
   @classmethod
   def queryZip(cls, zip):
-    return cls.query( cls.zipcode==zip).order(-cls.dateTime)
+    return cls.query( cls.zipcode==zip) #.order(-cls.dateTime)
 
 
 class QueryWeather(webapp2.RequestHandler):
-   def get(self):
+   def post(self):
       zipcode = self.request.get('zipcode')
       weaths = WeatherEntry.queryZip(zipcode).fetch(20)
       self.response.out.write(len(weaths))
@@ -27,20 +26,20 @@ class QueryWeather(webapp2.RequestHandler):
       for weath in weaths: 
          obj = {
            'zipcode': weath.zipcode, 
-           'date': weath.date,
+           #'date': weath.date,
            'temperature' : weath.temp
          } 
          self.response.out.write(json.dumps(obj))
      
 
 class InsertWeather(webapp2.RequestHandler):  
-  def get(self):
+  def post(self):
     zip = self.request.get('zipcode')
     temp = self.request.get('temp')
-    date = self.request.get('date')
+    #date = self.request.get('date')
     weath = WeatherEntry(parent=ndb.Key("weather", "yea"))
     weath.zipcode=zip
-    weath.date=date
+    #weath.date=date
     weath.temp=temp
     weath.put()
 
